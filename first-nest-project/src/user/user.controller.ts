fc88e881bf1e06ec313ba48e23d6c1c35c6e55ee
Sodiq Farhan <sodiqfarhan360@gmail.com>
2024-01-27 11:38:51 +0100

    Set up first nestjs project

---
+++ first-nest-project/src/user/user.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    // return this.userService.create(createUserDto);
+    return createUserDto
+  }
+
+  @Get()
+  findAll(@Query('role') role?: string) {
+    // return this.userService.findAll();
+    if (role) {
+      return {role, users: []}
+    }
+    return []
+  }
+
+  @Get('profile')
+  getProfile(){
+    return 'This is your profile'
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // return this.userService.findOne(+id);
+    return {id}
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    // return this.userService.update(+id, updateUserDto);
+    return {id, ...updateUserDto}
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    // return this.userService.remove(+id);
+    return {id}
+  }
+}
